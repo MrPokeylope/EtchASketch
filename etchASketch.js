266c71316c85b91834bf31e13e511dcbfc68bb04
Taylor Brown <taybrown9000@gmail.com>
2020-09-11 13:13:45 -0700

    Implement cell coloring and reset button

--- etchASketch.js
+++ etchASketch.js
@@ -1,16 +1,47 @@
-const gridContainer = document.getElementById('grid');
 
-function createGrid() {
+function createGrid(totalCells) {
+    
+    const gridContainer = document.getElementById('grid');
+    gridContainer.innerHTML = '';
+    // let gridMaxColumns = Math.floor(Math.sqrt(totalCells));
+    // console.log(gridMaxColumns);
 
-    let divArray = [];
+    gridContainer.gridTemplateColumns = `repeat(${totalCells}, 1fr)`;
+    gridContainer.gridTemplateRows = `repeat(${totalCells}, 1fr)`;
 
-    for (let i = 0; i < 16; i++) {
-        divArray[i] = document.createElement('div');
-        divArray[i].classList.add('gridItem');
-        divArray[i].textContent = i+1;
-        gridContainer.appendChild(divArray[i]);
+    let gridCells = [];
+
+    let findSize = Math.floor(Math.random() * totalCells) + 1;
+    console.log(findSize);
+
+    for (let i = 0; i < totalCells; i++) {
+        gridCells[i] = document.createElement('div');
+        gridCells[i].classList.add('gridItem');
+
+        // gridCells[i].style.gridColumn = Math.floor()
+
+        gridCells[i].addEventListener('mouseenter', () => {
+            gridCells[i].classList.add('touched');
+        });
+
+        gridContainer.appendChild(gridCells[i]);
     }
 }
 
+function resetGrid() {
+    const gridItems = document.querySelectorAll('.gridItem');
 
-createGrid();
+    gridItems.forEach(cell => {
+        cell.classList.remove('touched');
+    });
+
+    let numCells = prompt("How many squares per side should" + 
+                        " the new grid have?");
+
+    createGrid(numCells);
+}
+
+createGrid(16);
+
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', resetGrid);
