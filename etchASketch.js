ec741266403186c9eef8397a379643c3c3581d55
Taylor Brown <taybrown9000@gmail.com>
2020-09-12 18:29:27 -0700

    Complete program

--- etchASketch.js
+++ etchASketch.js
@@ -1,4 +1,28 @@
 
+function getRGBValue(gridCell) {
+
+    let RGB = [];
+
+    if (gridCell.id == 0) {
+        for (let i = 0; i < 3; i++) {
+            RGB[i] = Math.round(Math.random() * 256);
+        }
+    }
+    else {
+        RGB = gridCell.style.backgroundColor;
+        // rgb(164, 214, 116)
+        
+        RGB = RGB.replace('rgb(', '').replace(')', '').split(',');
+        
+        for (let i = 0; i < 3; i++) {
+            RGB[i] -= 26;
+        }
+    }
+    
+    ++gridCell.id;
+    return `rgb(${RGB[0]}, ${RGB[1]}, ${RGB[2]})`;
+}
+
 function createGrid(cellsPerRow) {
     
     const gridContainer = document.getElementById('grid');
@@ -7,11 +31,11 @@ function createGrid(cellsPerRow) {
     let gridWidth = gridContainer.offsetWidth;
     let cellSize = `${gridWidth / cellsPerRow}px`;
 
-    console.log(`
-    gridWidth: ${gridWidth}
-    cellsPerRow: ${cellsPerRow}
-    cellSize: ${gridWidth / cellsPerRow}
-    `);
+    // console.log(`
+    // gridWidth: ${gridWidth}
+    // cellsPerRow: ${cellsPerRow}
+    // cellSize: ${gridWidth / cellsPerRow}
+    // `);
 
     let gridCSS = `repeat(${cellsPerRow}, ${cellSize})`;
     gridContainer.style.gridTemplateColumns = gridCSS;
@@ -22,9 +46,14 @@ function createGrid(cellsPerRow) {
     for (let i = 0; i < Math.pow(cellsPerRow, 2); i++) {
         gridCells[i] = document.createElement('div');
         gridCells[i].classList.add('gridItem');
+        gridCells[i].id = '0';
 
         gridCells[i].addEventListener('mouseenter', () => {
-            gridCells[i].classList.add('touched');
+            if (gridCells[i].id < 10)
+                gridCells[i].style.backgroundColor = getRGBValue(gridCells[i]);
+            else 
+                return
+
         });
 
         gridContainer.appendChild(gridCells[i]);
