f40f3843f05c152eb5691643c2ad386b60863d13
Taylor Brown <taybrown9000@gmail.com>
2020-10-15 18:50:38 -0700

    Fix grid resizing and cell color reduce algorithm

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # EtchASketch
 
-A rudimentary Etch-A-Sketch clone written in Javascript. Each grid cell will change random colors when you mouse over them, getting darker each time until they eventually turn black. Click the "Reset Grid" button to clear the grid and change the grid size.
+A rudimentary Etch-A-Sketch clone written in Javascript. Each grid cell will change random colors when you mouse over them, getting darker each time until they eventually turn black. Click the "Reset Grid" button to clear the grid and change the grid size (to a max of 100 cells).
--- etchASketch.js
+++ etchASketch.js
@@ -1,21 +1,53 @@
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', resetGrid);
+
+createGrid(16);
+
+function resetGrid() {
+    let numCells = prompt("How many cells per row should the grid have? (Max is 100)");
+
+    if (numCells > 100) {
+        alert("Max number of cells is 100! Making a 100 cell grid...");
+        createGrid(100);
+    } else {
+        createGrid(numCells);
+    }
+}
 
 function getRGBValue(gridCell) {
 
     let RGB = [];
 
+    // format gridCell with initial color values
     if (gridCell.id == 0) {
+
+        // get random RGB values for background color
         for (let i = 0; i < 3; i++) {
             RGB[i] = Math.round(Math.random() * 256);
         }
-    }
-    else {
+
+        let reduceAmt = [];
+
+        // find amount to reduce each color by (10%)
+        for (let i = 0; i < 3; i++) {
+            reduceAmt[i] = Math.round(RGB[i] * .10);
+        }
+    
+        gridCell.setAttribute('data-reduce', `${reduceAmt[0]},${reduceAmt[1]},${reduceAmt[2]}`);
+
+    } else {
+
+        // get background color
+        // ex: rgb(164, 214, 116)
         RGB = gridCell.style.backgroundColor;
-        // rgb(164, 214, 116)
-        
         RGB = RGB.replace('rgb(', '').replace(')', '').split(',');
+
+        // get reduce percentage
+        let reduceAmt = gridCell.getAttribute('data-reduce');
+        reduceAmt = reduceAmt.split(',');
         
         for (let i = 0; i < 3; i++) {
-            RGB[i] -= 26;
+            RGB[i] -= reduceAmt[i];
         }
     }
     
@@ -24,22 +56,27 @@ function getRGBValue(gridCell) {
 }
 
 function createGrid(cellsPerRow) {
-    
-    const gridContainer = document.getElementById('grid');
-    gridContainer.innerHTML = '';
 
-    let gridWidth = gridContainer.offsetWidth;
+    const grid = document.getElementById('grid');
+
+    // clear the grid, if one already exists
+    if (grid.innerHTML !== '')
+        grid.innerHTML = '';
+
+    let gridWidth = grid.offsetWidth;
+    let gridHeight = grid.offsetHeight;
+    
     let cellSize = `${gridWidth / cellsPerRow}px`;
 
-    // console.log(`
-    // gridWidth: ${gridWidth}
-    // cellsPerRow: ${cellsPerRow}
-    // cellSize: ${gridWidth / cellsPerRow}
-    // `);
+    console.log(`Before:
+    gridWidth: ${gridWidth}
+    gridHeight ${gridHeight}
+    cellSize: ${cellSize}
+    `);
 
     let gridCSS = `repeat(${cellsPerRow}, ${cellSize})`;
-    gridContainer.style.gridTemplateColumns = gridCSS;
-    gridContainer.style.gridTemplateRows = gridCSS;
+    grid.style.gridTemplateColumns = gridCSS;
+    grid.style.gridTemplateRows = gridCSS;
 
     let gridCells = [];
 
@@ -49,25 +86,13 @@ function createGrid(cellsPerRow) {
         gridCells[i].id = '0';
 
         gridCells[i].addEventListener('mouseenter', () => {
-            if (gridCells[i].id < 10)
+            if (gridCells[i].id <= 10) {
                 gridCells[i].style.backgroundColor = getRGBValue(gridCells[i]);
-            else 
-                return
-
+            } else {
+                return;
+            }
         });
 
-        gridContainer.appendChild(gridCells[i]);
+        grid.appendChild(gridCells[i]);
     }
 }
-
-function resetGrid() {
-    let numCells = prompt("How many cells per row should" + 
-                        " the grid have?");
-
-    createGrid(numCells);
-}
-
-createGrid(16);
-
-const resetBtn = document.querySelector('#reset');
-resetBtn.addEventListener('click', resetGrid);
